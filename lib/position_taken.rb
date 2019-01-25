160e3ddf4bb3ffd49f7df4a1fbefb3b01d9146bb
Christopher Luna <chris@illusionlayervfx.com>
2019-01-25 18:52:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,8 @@
 def position_taken?(board, index)
   if board[index] == "X" || "O"
    true
- #elsif board[index] == "X" || "O"
-  #false
- else
-   false
+ elsif board[index] == "X" || "O"
+  false
+
  end
 end
